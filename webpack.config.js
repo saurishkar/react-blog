e3ce02eea46a24ddffd0bdaa485530746a5f51e1
vinsol <basicallysaurish@gmail.com>
2017-09-29 14:18:10 +0530

    added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
 	},
+	devtool: 'inline-source-maps',
 	module: {
 		rules: [
 			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
