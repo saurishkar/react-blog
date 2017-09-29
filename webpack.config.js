7bc0c896901e1e4363acb09421ac581054d6f2b8
vinsol <basicallysaurish@gmail.com>
2017-09-29 13:38:39 +0530

    webpack config setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
-	entry: './react-ecommerce-app/index.js'
-}
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+		]
+	}
+};
