52933ef14fab853a861c63c186a74314abb3c2ef
vinsol <basicallysaurish@gmail.com>
2017-10-13 14:33:32 +0530

    added posts view to home page

--- src/components/admin/home.js
+++ src/components/admin/home.js
@@ -8,10 +8,35 @@ import { FetchPosts } from '../../actions/posts';
 class Home extends Component {
 	constructor(props) {
 		super(props);
+
+		this.renderPosts = this.renderPosts.bind(this);
 	}
 
-	componentDidMount() {
+	componentWillMount() {
 		this.props.FetchPosts();
+	}
+
+	renderPosts() {
+
+		return Object.entries(this.props.posts).map((elem, index) => {
+			return (
+				<div className="panel panel-default" key={index}>
+					<div className="panel-heading">
+						<div className="row">	
+							<div className="col-sm-8">
+								{elem[1].title}
+							</div>
+							<div className="col-sm-4" style={{ textAlign: 'right'}}>
+								#{index}
+							</div>
+						</div>
+					</div>
+					<div className="panel-content">
+						<p>{elem[1].content}</p>
+					</div>
+				</div>
+			);
+		});
 	}
 
 	render() {
@@ -19,8 +44,9 @@ class Home extends Component {
 			<div className="home">
 				<NavbarMain />
 				<div className="container">
-					<h3 className="text-center"> This is the Home Page of the application </h3>
-					<div className="pane">
+					<h3 className="text-center"> Latest Posts </h3>
+					<div className="blog-body">
+						{ this.renderPosts() }
 					</div>
 				</div>
 			</div>
