e9dd1791d40a38a2de2614da55c4db3fd52bd9e5
vinsol <basicallysaurish@gmail.com>
2017-11-05 04:13:57 +0530

    main page design created

--- src/components/admin/home.js
+++ src/components/admin/home.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import {bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
+import { Button } from 'material-ui';
 
 import NavbarMain from '../shared/navbar_main';
 import { FetchAllPosts } from '../../actions/posts';
@@ -23,24 +24,26 @@ class Home extends Component {
 				return (
 					<div className="col-sm-12" key={index}>
 						<div className="jumbotron">
+							<div className="post-head">
+								<h2>{elem[1].title}</h2>
+								<span>
+									<small>	{elem[1].author_email}</small>&nbsp;
+									<small><i>{elem[1].last_updated}</i></small>
+								</span><br/>
+							</div>
+								
 							<div className="row">
-								<div className="col-sm-8">
-									<div className="post-head">
-										<h2>{elem[1].title}</h2>
-										<span>
-											<small>	{elem[1].author_email}</small>&nbsp;
-											<small><i>{elem[1].last_updated}</i></small>
-										</span><br/>
+								<div className="col-sm-6">
+									<div className="post-body">
+										<p>{elem[1].content}</p>
+										<p></p>
 									</div>
 								</div>
-								<div className="col-sm-4">
-									<img alt="sample-post-image" />
+								<div className="col-sm-6">
+									<img alt="sample-post-image" src="https://s6.postimg.org/t5bdqrlc1/blog-post.png"/>
 								</div>
 							</div>
-							<div className="post-body">
-								<p>{elem[1].content}</p>
-								<p></p>
-							</div>
+							<Button raised color="primary">Read More</Button>
 						</div>
 					</div>
 				);
@@ -52,8 +55,9 @@ class Home extends Component {
 		return (
 			<div>
 				<div className="header-nav"><NavbarMain /></div>
-				<div className="container">
-					<h3>Latest Feed</h3>
+				<div className="general-feed">
+					<h3 style={{ color: '#777' }} >Latest Feed</h3>
+					<br />
 					<div className="posts">
 						<div className="row">{ this.renderPosts() }</div>
 					</div>
--- src/components/main.js
+++ src/components/main.js
@@ -4,8 +4,20 @@ class Main extends Component{
 
 	render() {
 		return (
-			<div>
-				<h2>Some Awesomeness in Progress....</h2>
+			<div className="page-body">
+				<div className="main">
+					<div className="quad">
+						<div className="sub-quad">
+							<div className="sub-quad-1"></div>
+							<div className="sub-quad-2"></div>
+							<div className="sub-quad-3"></div>
+							<div className="sub-quad-4"></div>
+						</div>
+						<h1>REACT</h1>
+						<h4> BLOG </h4>
+					</div>
+					<h3>THE</h3>
+				</div>
 			</div>
 		);
 	}
